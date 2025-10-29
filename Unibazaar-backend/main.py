747f2e304b86edc99ebe76826ee4f0cedbd765b4
syeda ummefizza <162727504+syedafizza410@users.noreply.github.com>
2025-10-30 00:56:57 +0500

    add

--- Unibazaar-backend/main.py
+++ Unibazaar-backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="UniBazaar Backend", version="1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://unibazaar-kappa.vercel.app/"],  
+                   "https://unibazaar-kappa.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
